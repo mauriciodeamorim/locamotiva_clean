8271f72b5f58496fc7ac307437b580b1412d1327
Mauricio de Amorim <amorim@timedev.com>
2009-05-20 00:22:26 -0300

    Atualizações no controller de Resultados.

--- app/controllers/resultados_controller.rb
+++ app/controllers/resultados_controller.rb
@@ -1,2 +1,16 @@
 class ResultadosController < ApplicationController
+  def index
+    render :text => "Primeiro teste com controller!"
+  end
+
+  def ano
+    @ano = Atleta.find(:all)
+
+#    respond_to do |format|
+#      format.html
+#    end
+
+    render :text => @ano
+  end
 end
+
