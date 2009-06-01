e9df6dfe238ee3ee7b46ea36f0c07ea444b41769
Mauricio de Amorim <amorim@timedev.com>
2009-06-01 09:30:19 -0300

    Inclusao de view para mostrar resultados por prova.

--- app/controllers/resultados_controller.rb
+++ app/controllers/resultados_controller.rb
@@ -11,7 +11,7 @@ class ResultadosController < ApplicationController
     #@last_competition = Prova.find(:last)
     @last_competition = last_competition()
     @results = Resultado.find(:all, :conditions => ["idProva = #{@last_competition.id}"])
-    @all_competitions = all_competitions
+    @all_competitions = all_competitions_less_last
 
     respond_to do |format|
       format.html # index.html.erb
@@ -19,9 +19,19 @@ class ResultadosController < ApplicationController
     end
   end
 
-  def all_competitions
-    Prova.find(:all)
+  def all_competitions_less_last
+    Prova.find(:all, :conditions => ["id != #{last_competition().id}"], :order => "data DESC")
 #    Resultados.find([params:idProva])
+  end
+
+  def show
+    @all_results = Resultado.find(:all, :conditions => ["idProva = #{params[:id]}"])
+#    @prova = Prova.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @all_results }
+    end
   end
 end
 
