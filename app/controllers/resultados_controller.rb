023217529b6d3be01042758adf4a3f5d057da290
Mauricio de Amorim <amorim@timedev.com>
2009-06-01 06:59:02 -0300

    Exibicao dos resultados por prova.

--- app/controllers/resultados_controller.rb
+++ app/controllers/resultados_controller.rb
@@ -1,9 +1,9 @@
 class ResultadosController < ApplicationController
   def index
-    resultadosPorProva
+  results_last_competition
   end
 
-  def resultadosPorProva
+  def last_competition
     @resultado = Resultado.new
 
     @resultado.prova = Prova.find(:last)
@@ -13,6 +13,21 @@ class ResultadosController < ApplicationController
       format.xml  { render :xml => @resultado }
     end
 #    Resultados.find([params:idProva])
+  end
+
+  def results_last_competition
+    @results = Resultado.new
+    @last_competition = Prova.find(:last)
+    @results.prova = Prova.find(:last)
+
+    id_prova = @results.prova.id
+
+    @results = Resultado.find(:all, :conditions => ["idProva = #{id_prova}"])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @results }
+    end
   end
 end
 
